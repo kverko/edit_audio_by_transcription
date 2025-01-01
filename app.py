3d6b54147cd4e8c437fa03594d4653652a6769c6
S. A. Rubiszewski <slawomirr@gmail.com>
2025-01-01 20:56:15 +0100

    look for marks in words, not only on beginnings and ends

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from io import BytesIO
-import re
-import time
 
 import streamlit as st
 from audiorecorder import audiorecorder
@@ -13,19 +11,6 @@ AUDIO_TRANSCRIBE_MODEL = "whisper-1"
 
 def get_openai_client():
     return OpenAI(api_key=st.session_state["openai_api_key"])
-
-
-# def transcribe_audio_to_text(audio_path):
-#     openai_client = get_openai_client()
-#     with open(audio_path, "rb") as f:
-#         transcript = openai_client.audio.transcriptions.create(
-#             file=f,
-#             model="whisper-1",
-#             response_format="verbose_json",
-#         )
-
-#     return transcript.text
-
 
 def transcribe_audio(audio_bytes):
     openai_client = get_openai_client()
@@ -82,8 +67,6 @@ if new_audio:
     st.session_state["new_audio_bytes"] = audio.getvalue()
     st.audio(audio, format="audio/mp3")
     transcription = transcribe_audio(st.session_state["new_audio_bytes"])
-    # if transcription != st.session_state["new_audio_text"]:
-    #     st.session_state["new_audio_text"] = st.session_state["transcription"]
 
 new_text = st.text_area(
     "Transkrypcja (zaznacz fragment, który chcesz usunąć z audio)", 
@@ -91,29 +74,25 @@ new_text = st.text_area(
 )
 text_with_edits = new_text
 if st.button("Zapisz zmiany"):
-    # st.text(new_text)
-    # marked_words = re.findall(r'\[\[(.*?)\]\]', new_text)
-    # for word in marked_words:
-    #    st.text(word)
-    
-
-
     words = new_text.split(' ')
     rem_starts = []
     rem_ends = []
     
-    for idx, word in enumerate(words):
-        if word.startswith("[["):
-            rem_starts.append(transcription['words'][idx]['start']*1000)
-        if word.endswith("]]"):
+    for idx, word in enumerate(words):        
+        if '[[' in word:
+            rem_starts.append(transcription['words'][idx]['start']*1000)        
+        if ']]' in word:
             rem_ends.append(transcription['words'][idx]['end']*1000)
 
     
     temp_audio = AudioSegment.from_file(audio)
     output_audio = AudioSegment.empty()
+    last_end = 0
     for start, end in zip(rem_starts, rem_ends):
         print(start, end)
-        output_audio += temp_audio[start:end]
+        output_audio += temp_audio[last_end:start]
+        last_end = end
+    output_audio += temp_audio[last_end:]
     
     output_audio_bytes = BytesIO()
     output_audio.export(output_audio_bytes, format="mp3")
